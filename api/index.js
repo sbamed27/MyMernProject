2698f65e2e54ecc9acedc5a1c11c9e21d1ff2601
Mohamed Aminov <sbamed27@users.noreply.github.com>
2024-01-22 01:30:26 +0100

    Connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
+mongoose.connect(process.env.mongo).then(
+    ()=>{
+        console.log("MongoDB is connected thru Aminov");
+    }
+).catch(
+    (error)=>{
+        console.log(error);
+    }
+);
+
 app.listen(3000, ()=>{
-    console.log('Server is running in port 3000 !!!!');
+    console.log('Server is running in port 3000 ');
 })
